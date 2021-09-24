fc17d94dcf91be34d0f3e9216ff7dcaa62652808
hckme4 <hckme4@protonmail.com>
2021-09-24 16:12:50 -0400

    added macros.h for #define directives, began work on main.c

---
+++ src/macros.h
@@ -0,0 +1,12 @@
+#ifndef _MACROS_H_
+#define _MACROS_H_
+
+/* All define statements used across all systems */
+
+/* Booleans */
+#define TRUE 1
+#define FALSE 0
+
+
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,14 @@
+/******************************************************************************
+ * C0rruptC Preprocessor
+ *
+ * Code to replace C Preprocessor macro code with actual C code that may be
+ * compiled, attempting to comply with C89 standards (or C99 if goal is not
+ * achievable).
+ *
+ * Start: 24 Sep 2021
+ * End:
+ *****************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
